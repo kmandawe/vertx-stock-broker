c0cb4b5fb39de8c5e9429051d92d8606930ce9e6
kmandawe <kdmandawe06@gmail.com>
2022-04-11 16:46:25 +0800

    PgPool Configuration and SELECT

--- src/main/java/com/kensbunker/vertx/broker/assets/AssetsRestApi.java
+++ src/main/java/com/kensbunker/vertx/broker/assets/AssetsRestApi.java
@@ -1,6 +1,7 @@
 package com.kensbunker.vertx.broker.assets;
 
 import io.vertx.ext.web.Router;
+import io.vertx.pgclient.PgPool;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,7 +11,8 @@ public class AssetsRestApi {
   public static final List<String> ASSETS =
       Arrays.asList("AAPL", "AMZN", "FB", "GOOG", "MSFT", "NFLX", "TSLA");
 
-  public static void attach(Router parent) {
+  public static void attach(Router parent, final PgPool db) {
     parent.get("/assets").handler(new GetAssetsHandler());
+    parent.get("/pg/assets").handler(new GetAssetsFromDatabaseHandler(db));
   }
 }
